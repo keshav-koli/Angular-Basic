5e8212428b956517fd4970afaaf0f13e233fdb1b
keshav-koli <keshavkoli5588@gmail.com>
2025-04-19 15:28:35 +0530

    Refactor app component and styles for counter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,39 @@ import { signUpComponent } from './signUp/signUp.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent,signUpComponent],
+  imports: [RouterOutlet, LoginComponent, signUpComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  name='asdf'
+  counter: number = 0;
+  name: string = 'dfs';
+  sname: string | number | boolean = 'asdf';
   title = 'first-angular by keshav';
+
+  handleButton() {
+    console.log('hello Ji...');
+    this.handle2Button();
+    this.sum(4, 8);
+    console.log(this.sum(8, 7));
+  }
+  increment() {
+    this.counter += 1;
+  }
+  decrement() {
+    this.counter -= 1;
+  }
+  reset() {
+    this.counter = 0;
+  }
+
+  handle2Button() {
+    this.sname = 'asd';
+    this.sname = 78;
+    console.log('Namaste Ji....');
+  }
+
+  sum(x: number, y: number) {
+    console.log(x + y);
+  }
 }
