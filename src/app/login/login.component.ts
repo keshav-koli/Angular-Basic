59bce76ab71a4c3c39c888f783059c6d7bf6f70d
keshav-koli <keshavkoli5588@gmail.com>
2025-04-20 00:23:06 +0530

    Enhance login and sign-up components with input handling and event logging

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,8 +4,20 @@ import { Component } from '@angular/core';
   selector: 'login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  $input!: InputEvent;
+  handleEvent(event: MouseEvent | any) {
+    console.log('Login Button is clicked');
+    console.log('Function :', event);
+    console.log('Function :', event.type);
+    console.log('Function :', event.target);
+    console.log('CLASS IS : ', (event.target as Element).className);
+  }
+  handleEventInput(event: Event) {
+    console.log('Function :', event);
+    console.log('Function :', event.target);
+    console.log('Function :', (event?.target as HTMLInputElement).value);
+  }
 }
--- src/app/signUp/signUp.component.ts
+++ src/app/signUp/signUp.component.ts
@@ -1,12 +1,30 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:"signUp",
-    imports:[],
-    styles:'',
-    template:`<h1>Sign Up Works!!</h1>`
+  selector: 'signUp',
+  imports: [],
+  styles: '',
+  templateUrl: './signUp.Component.html',
 })
-export class signUpComponent{
-    
-}
+export class signUpComponent {
+  sname: string = '';
+  DisplayName = '';
+  email = '';
+  handleName(event: Event) {
+    // console.log((event.target as HTMLInputElement).value);
+    this.DisplayName = (event.target as HTMLInputElement).value;
+  }
+  getName() {
+    this.sname = this.DisplayName;
+  }
+  setName() {
+    this.DisplayName = 'Default Name';
+  }
 
+  getEmail(email: string) {
+    this.email = email;
+  }
+  setEmail() {
+    this.email = 'default@gmail.com';
+  }
+}
